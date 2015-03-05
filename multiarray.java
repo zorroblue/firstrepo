b8da3c57287519694bda1f22773370ea002255c7
Rameshwar Bhaskaran <rameshwar.zorro@gmail.com>
2015-03-05 19:12:10 +0530

    Added all files now

---
+++ multiarray.java
@@ -0,0 +1,9 @@
+class multiarray
+{
+public static void main(String[] args)
+{
+String[][] names={{"I ","Rameshwar "},{"am","Bhaskaran"}};
+System.out.println(names[0][0]+names[1][0]+names[0][1]+names[1][1]);
+}
+}
+
